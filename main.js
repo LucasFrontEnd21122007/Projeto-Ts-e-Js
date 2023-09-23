99c08faf90b66a17d6c5d0d1e2e9d09d707516ca
LUCAS MARTINEZ <132151483+LucasFrontEnd21122007@users.noreply.github.com>
2023-09-23 11:28:10 -0300

    arquivos do projeto
    arquivos do projeto finalizado

---
+++ main.js
@@ -0,0 +1,10 @@
+// main.ts
+import { add } from './calculator';
+
+const num1 = 5;
+const num2= 10;
+
+const resultado = 5+10
+console.log("o resultado é 15")
+print(resultado) = 15
+console.log(`A soma de ${num1} e ${num2} é igual a 15`);
---
+++ main.ts
@@ -0,0 +1,7 @@
+// calculator.ts
+function add(a: number, b: number): number {
+    return a + b;
+  }
+  
+  export { add };
+  
